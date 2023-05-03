4ab57a20756e965dc32afdaa98e3d154c08549ea
Okeke-Stephen <113146022+Okeke-Stephen@users.noreply.github.com>
2023-05-04 02:06:15 +1200

    Create app.py

---
+++ app.py
@@ -0,0 +1,60 @@
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+# loading the saved models
+ins_model = pickle.load(open('insurance_model.sav', 'rb'))
+
+# page title
+st.title('Insurance Predictive Framework')
+    
+    
+# getting the input data from the user
+col1, col2, col3 = st.columns(3)
+    
+with col1:
+  age = st.text_input('Age')
+        
+with col2:
+        if st.checkbox('sex'):
+            smoker = 'male'
+        else:
+            smoker = 'female' 
+    
+with col3:
+  bmi = st.text_input('Body Mass Index')
+    
+with col1:
+  children = st.sidebar.slider('Children', min_value=0, max_value=10, value=0, step = 1)
+    
+with col2:
+        if st.checkbox('Smoker'):
+            smoker = 'yes'
+        else:
+            smoker = 'no'
+    
+with col3:
+  region = st.text_input('Region')
+  if (region == 'region_northwest'):
+    region_northwest = 1
+    region_southeast = 0
+    region_southwest = 0
+        
+  elif (region == 'region_southeast'):
+      region_northwest = 0
+      region_southeast = 1
+      region_southwest = 0
+
+  else:
+      region_northwest = 0
+      region_southeast = 0
+      region_southwest = 1
+
+# creating a button for Prediction
+
+# creating a button for Prediction
+if st.button("Predict"):
+    output = ins_model.predict([[age, sex,	bmi,	children,	smoker,	region]])
+    output = '$' + str(output)
+    st.success('The insurance price estimate is: {}'.format(output))
